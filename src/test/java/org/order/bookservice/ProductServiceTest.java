eaeea28b85d61a8f3052e62eed4b4645d7e80bad
Kirill Bychkov <k.bychkov@btslogistics.by>
2025-03-27 17:24:23 +0300

    temp

--- src/test/java/org/order/bookservice/ProductServiceTest.java
+++ src/test/java/org/order/bookservice/ProductServiceTest.java
@@ -3,6 +3,7 @@ package org.order.bookservice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,13 +11,19 @@ import org.order.bookservice.dto.OrderResponse;
 import org.order.bookservice.dto.ProductRequest;
 import org.order.bookservice.dto.ProductResponse;
 import org.order.bookservice.entity.Category;
+import org.order.bookservice.entity.Order;
 import org.order.bookservice.entity.Product;
 import org.order.bookservice.mapper.ProductMapper;
+import org.order.bookservice.repository.OrderRepository;
 import org.order.bookservice.repository.ProductRepository;
+import org.order.bookservice.service.CurrencyService;
 import org.order.bookservice.service.ProductService;
 import org.order.bookservice.utils.CurrencyConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
@@ -36,18 +43,24 @@ class ProductServiceTest {
     @Mock
     private ProductRepository productRepository;
 
-    @Mock
-    private ProductMapper productMapper;
+    private final ProductMapper productMapper = Mappers.getMapper(ProductMapper.class);
 
     @Mock
     private CurrencyConverter currencyConverter;
 
-    @InjectMocks
+    @Mock
+    private CurrencyService currencyService;
+
     private ProductService productService;
 
     private Product productUSD;
 
     private Product productEUR;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    private Order order;
 
     private ProductResponse productResponse;
 
@@ -56,6 +69,12 @@ class ProductServiceTest {
         productUSD = new Product(1L, "Test Product", new BigDecimal("100.00"), "USD", new Category(1L, "Books"), new HashSet<>());
         productEUR = new Product(2L, "Test Product2", new BigDecimal("50.00"), "EUR", new Category(1L, "Books"), new HashSet<>());
         productResponse = new ProductResponse(1L, "Test Product", new BigDecimal("100.00"), "USD", "Books", "Fiction");
+        productService = new ProductService(productRepository, orderRepository, productMapper, currencyConverter);
+
+        order = new Order();
+        order.setId(1L);
+        order.setCurrency("RUB");
+        order.setOrderProducts(new HashSet<>());
     }
 
     @Test
@@ -65,7 +84,6 @@ class ProductServiceTest {
         Pageable pageable = Pageable.unpaged();
 
         when(productRepository.findAll(specification, pageable)).thenReturn(productList);
-        when(productMapper.toDTO(productUSD)).thenReturn(productResponse);
         when(currencyConverter.convert(new BigDecimal("100.00"), "USD", "RUB"))
                 .thenReturn(new BigDecimal("7500.00"));
 
@@ -88,6 +106,7 @@ class ProductServiceTest {
                 .thenReturn(new BigDecimal("7500.00"));
         when(currencyConverter.convert(new BigDecimal("50.00"), "EUR", "RUB"))
                 .thenReturn(new BigDecimal("5000.00"));
+        when(orderRepository.save(any(Order.class))).thenReturn(order);
 
         OrderResponse response = productService.makeOrder(request);
 
@@ -97,17 +116,19 @@ class ProductServiceTest {
 
         verify(currencyConverter, times(1)).convert(new BigDecimal("100.00"), "USD", "RUB");
         verify(currencyConverter, times(1)).convert(new BigDecimal("50.00"), "EUR", "RUB");
+        verify(orderRepository, times(1)).save(any(Order.class));
     }
 
     @Test
     void testMakeOrderWhenCurrencyConversionFails() {
         ProductRequest request = new ProductRequest(List.of(1L, 2L), "RUB");
         when(productRepository.findAllById(List.of(1L, 2L))).thenReturn(List.of(productUSD, productEUR));
-        when(currencyConverter.convert(any(), any(), any())).thenThrow(new IllegalArgumentException("Currency conversion service unavailable"));
+        when(currencyService.getExchangeRates()).thenThrow(new IllegalArgumentException("Currency conversion service unavailable"));
 
         Exception exception = assertThrows(IllegalArgumentException.class, () -> productService.makeOrder(request));
 
         assertEquals("Currency conversion service unavailable", exception.getMessage());
-        verify(currencyConverter, times(1)).convert(any(), any(), any());
+        verify(currencyService, times(1)).getExchangeRates();
     }
+
 }
